8797e4a59fe0fc3fa3d14b7f9d46fba91e33b24f
dat <dathalongbay@gmail.com>
2022-09-08 19:23:14 +0700

    111

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+
+    <Greeting />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Greeting from './components/Greeting.vue';
+import Greeting from './components/Greeting.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Greeting,
+    Greeting
+}
 }
 </script>
 
